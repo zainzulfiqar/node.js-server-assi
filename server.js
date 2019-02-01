f2d13f5fed20432c81c9b63681ca9a283dd2b06d
zainzulfiqar <chzainkhan13@gmail.com>
2019-02-01 12:10:16 +0500

    port change

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const server = require('./app.js')
 
-server.listen(8000 || process.env.PORT, ()=>{
+server.listen(process.env.PORT, ()=>{
     console.log('Server is running on port ' + 8000);
 });
