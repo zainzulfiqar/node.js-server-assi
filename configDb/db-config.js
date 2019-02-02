c662c97cc60293a9650e7a687d69fab5ff80fac7
zainzulfiqar <chzainkhan13@gmail.com>
2019-02-02 09:18:40 +0500

    db

--- configDb/db-config.js
+++ configDb/db-config.js
@@ -1,8 +1,8 @@
 let mongoose = require('mongoose');
 
-let db_url = 'mongodb://zainkhan:z6060750@ds219055.mlab.com:19055/users';
-mongoose.connect(db_url, { useNewUrlParser: true })
+let db_url = "mongodb://zainkhan:z6060750@ds219055.mlab.com:19055/users";
 
-let db =mongoose.connection;
+mongoose.connect(db_url, { useNewUrlParser: true });
+var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'DB connection error:'));
 db.once('open', function () { console.log('Successfully connected to DB') });
